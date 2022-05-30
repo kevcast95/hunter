5964f9851c04393037a11b48a123253aba702e72
kevcast95 <kevcast06@gmail.com>
2022-05-30 16:57:53 -0500

    adding baseURL

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route exact path="/" element={<Welcome />} />
           <Route exact path="/first" element={<Collar />} />
