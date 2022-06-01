826bee0b9bdf03b0de10f33214294975f587de5e
kevcast95 <kevcast06@gmail.com>
2022-05-31 23:42:57 -0500

    using div editable

--- src/components/Series/index.js
+++ src/components/Series/index.js
@@ -9,76 +9,70 @@ import nueve from '../../assets/nueve.png'
 import './Series.scss'
 
 function Series({ setIsOpen, setFailed }) {
-  const correct = ['10','9']
-  const [answer, setAnswer] = useState({
-    ms1: '',
-    ms2: ''
-  })
-  useEffect(() => {
-  if (answer.ms1.length > 0 && answer.ms2.length > 0) {
-    if (answer.ms1 === correct[0] && answer.ms2 === correct[1]) {
+  const correct = ['10', '9']
+
+  function validate() {
+    const ms1 = document.getElementById('ms1').textContent
+    const ms2 = document.getElementById('ms2').textContent
+    if (ms1 === correct[0] && ms2 === correct[1]) {
       setFailed(false)
-    }else {
+    } else {
       setFailed(true)
       setTimeout(() => {
         setIsOpen(false)
         setFailed(false)
-        setAnswer({
-          ms1: '',
-          ms2: ''
-        })
-      },3200)
+      }, 3200)
     }
     setIsOpen(true)
   }
-  },[answer])
 
   return (
-    <div className="series">
-      <div className="series-rows">
-        <img className="series-img" src={harry} alt="series-harry" />
-        <img className="series-img" src={harry} alt="series-harry" />
-        <img className="series-img" src={nueve} alt="series-nueve" />
-        =
-        <p className="series-results">10</p>
+    <>
+      <div className="series">
+        <div className="series-rows">
+          <img className="series-img" src={harry} alt="series-harry" />
+          <img className="series-img" src={harry} alt="series-harry" />
+          <img className="series-img" src={nueve} alt="series-nueve" />
+          =
+          <p className="series-results">10</p>
+        </div>
+        <div className="series-rows">
+          <img className="series-img" src={nueve} alt="series-nueve" />
+          <img className="series-img" src={reliquias} alt="series-reliquias" />
+          <img className="series-img" src={hogwart} alt="series-hogwart" />
+          =
+          <div 
+            className="series-answer" 
+            id="ms1"
+            contentEditable
+          >
+
+          </div>
+        </div>
+        <div className="series-rows">
+          <img className="series-img" src={nueve} alt="series-nueve" />
+          <img className="series-img" src={hogwart} alt="series-hogwart" />
+          <img className="series-img" src={harry} alt="series-harry" />
+          =
+          <p className="series-results">9</p>
+        </div>
+        <div className="series-rows">
+          <span className="series-num">=8</span>
+          <span className="series-num">=12</span>
+          <span className="series-num">
+            <div className="series-answer"
+              id="ms2"
+              contentEditable
+            >
+            </div>
+          </span>
+          <p className="series-hide">0</p>
+        </div>
       </div>
-      <div className="series-rows">
-        <img className="series-img" src={nueve} alt="series-nueve" />
-        <img className="series-img" src={reliquias} alt="series-reliquias" />
-        <img className="series-img" src={hogwart} alt="series-hogwart" />
-        = 
-        <input 
-          type="text" 
-          name="ms1" 
-          placeholder="?"
-          value={answer.ms1}
-          autocomplete="false"
-          onChange={(e)=> setAnswer({...answer, [e.target.name]: e.target.value})} 
-        />
-      </div>
-      <div className="series-rows">
-        <img className="series-img" src={nueve} alt="series-nueve" />
-        <img className="series-img" src={hogwart} alt="series-hogwart" />
-        <img className="series-img" src={harry} alt="series-harry" />
-        =
-        <p className="series-results">9</p>
-      </div>
-      <div className="series-rows">
-        <span className="series-num">=8</span>
-        <span className="series-num">=12</span>
-        <span className="series-num">
-          <input 
-            type="text" 
-            name="ms2" 
-            placeholder="?"
-            value={answer.ms2}
-            autocomplete="false"
-            onChange={(e)=> setAnswer({...answer,  [e.target.name]: e.target.value})} 
-          />
-        </span>
-        <p className="series-hide">0</p>
-      </div>
-    </div>
+      <button className="welcome-start" onClick={()=> validate()}>
+        Validar
+      </button>
+    </>
   )
 }
 
